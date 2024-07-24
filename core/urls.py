85aba9ec7adb7b95c9357ece1794618f458db237
Phạm Trung Đức <ducphamtrung.devw@gmail.com>
2024-07-24 15:15:57 +0700

    #8: Subscribing to channel feature using Django and Axios

--- core/urls.py
+++ core/urls.py
@@ -8,4 +8,8 @@ urlpatterns = [
   # Saving comment to db
   path("ajax-save-comment/", views.ajax_save_comment, name="save-comment"),
   path("ajax-delete-comment/", views.ajax_delete_comment, name="delete-comment"),
+
+  # Subscribe function
+  path("add-sub/<int:id>/", views.add_new_subscribers, name="add-sub"),
+  path("sub-load/<int:id>/", views.load_channel_subs, name="sub-load"),
 ]
--- core/views.py
+++ core/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 from django.db.models import Count, Q
 from django.views.decorators.csrf import csrf_exempt
 
+from channel.models import Channel
+
 from .models import Video, Comment
 
 # Create your views here.
@@ -14,6 +16,7 @@ def index(request):
   return render(request, "index.html", context)
 def videoDetail(request, pk):
   video = Video.objects.get(id=pk)
+  channel = Channel.objects.get(user=video.user)
   
   video.views = video.views + 1
   video.save()
@@ -30,6 +33,7 @@ def videoDetail(request, pk):
     "video": video,
     "similar_videos": similar_videos,
     "comment": comment,
+    "channel": channel,
   }
   return render(request, "video-detail.html", context)
 
@@ -56,3 +60,24 @@ def ajax_delete_comment(request):
     return JsonResponse({"status":1})
   else:
     return JsonResponse({"status":2})
+
+# Subscribe Functions
+def add_new_subscribers(request, id):
+  channel = Channel.objects.get(id=id)
+  user = request.user
+
+  if user in channel.subscribers.all():
+    channel.subscribers.remove(user)
+    response = "Subscribe"
+    return JsonResponse(response, safe=False, status=200)
+  else:
+    channel.subscribers.add(user)
+    response = "Unsubscribe"
+    return JsonResponse(response, safe=False, status=200)
+
+# Load channel subs
+def load_channel_subs(request, id):
+  channel = Channel.objects.get(id=id)
+  sub_lists = list(channel.subscribers.values())
+  return JsonResponse(sub_lists, safe=False, status=200)
+
