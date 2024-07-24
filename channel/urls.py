95532263e524e50cf64f5d71476001c80961ed07
Phạm Trung Đức <ducphamtrung.devw@gmail.com>
2024-07-24 23:49:09 +0700

    #11: Video page

--- channel/urls.py
+++ channel/urls.py
@@ -3,4 +3,5 @@ from channel import views
 
 urlpatterns = [
   path("<channel_name>/", views.channel_profile, name="channel-profile"),
+  path("<channel_name>/video/", views.channel_videos, name="channel-videos"),
 ]
--- channel/views.py
+++ channel/views.py
@@ -21,4 +21,15 @@ def channel_profile(request, channel_name):
     "video_featured": video_featured
   }
 
-  return render(request, "channel.html", context)
+  return render(request, "channel/channel.html", context)
+
+def channel_videos(request, channel_name):
+  channel = get_object_or_404(Channel, id=channel_name)
+  videos = Video.objects.filter(user=channel.user, visibility="public").order_by("-date")
+        
+  context = {
+      "videos":videos,
+      "channel":channel,
+  }
+
+  return render(request, "channel/channel-videos.html", context)
