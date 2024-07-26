e53c35a58a3c94066f1f6b312a3a738f6020f03c
Phạm Trung Đức <ducphamtrung.devw@gmail.com>
2024-07-26 09:53:51 +0700

    #16: Creating and automating user profile and Django signals

--- userauths/admin.py
+++ userauths/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from .models import User
+from .models import Profile, User
 from import_export.admin import ImportExportModelAdmin
 
 class UserAdmin(ImportExportModelAdmin):
     pass
 
+class ProfileAdmin(ImportExportModelAdmin):
+    list_display = ['user']
+
 admin.site.register(User, UserAdmin)
+admin.site.register(Profile, ProfileAdmin)
--- userauths/models.py
+++ userauths/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db.models.signals import post_save
+
+from core.models import Video
 
 class User(AbstractUser):
     email = models.EmailField(unique=True, null=True)
@@ -10,3 +13,20 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
+    
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
+    saved_videos = models.ManyToManyField(Video, null=True, blank=True, related_name="saved_videos")
+
+    def __str__(self):
+        return self.user.username
+    
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
+post_save.connect(create_user_profile, sender=User)
+post_save.connect(save_user_profile, sender=User)
